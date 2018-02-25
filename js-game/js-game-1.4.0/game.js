adbbd44eec9298a4668ad9ed4a95b0a699317fed
Lena Vladi <lenavladi.web@ya.ru>
2018-02-25 22:30:47 +0300

    class Vector

--- js-game/js-game-1.4.0/game.js
+++ js-game/js-game-1.4.0/game.js
@@ -1,1 +1,25 @@
 'use strict';
+class Vector {
+  constructor(x = 0, y = 0){
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(objVector){
+    try {
+      if(typeof objVector !== 'object'){
+        throw 'Можно прибавлять к вектору только вектор типа Vector.';
+      }
+      let x = objVector.x + this.x;
+      let y = objVector.y + this.y;
+      return new Vector(x, y);
+    } catch(e) {
+      alert(e);
+    }
+  }
+  times(num){
+    let x = num * this.x;
+    let y = num * this.y;
+    return new Vector(x, y);
+  }
+}
